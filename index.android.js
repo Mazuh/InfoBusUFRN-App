2e4c0624dec8140f4b9f2c96893ab0d9852a95dd
Mazuh <marcell-mz@hotmail.com>
2017-08-07 13:53:49 -0300

    Set app name and its icons

--- index.android.js
+++ index.android.js
@@ -17,14 +17,11 @@ export default class InfoBusUFRN extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Olá, mundo!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          Já comecei editando o arquivo index.android.js.
+          Depois de um quase dois dias preparando o ambiente!
         </Text>
       </View>
     );
