2da3238064f304c0342c884226201ba201b68bf8
yuriscosta <yuri.sales@protonmail.com>
2018-02-25 03:59:17 -0300

    Add function to return the reference's endpoints

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { retrieveAllSchedules } from './src/services/gist';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.data_gist_endpoint = 'https://gist.githubusercontent.com/Mazuh/e10c07f1abb580c143557d8ed8427bbd/raw/2a227ea93d5ea6bb06210dbc5bfb896f737a71ea/infobus_data.json';
+  }
+
+  componentDidMount() {
+    retrieveAllSchedules(this.data_gist_endpoint)
+      .then((data) => {
+        this.setState({
+          gist_data: data
+        });
+      });   
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{this.state.gist_data}</Text>
       </View>
     );
   }
